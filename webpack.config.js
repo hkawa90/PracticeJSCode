1f862e2747353ffddd429689aad6fac9406749ad
hkawa90 <hideo.90.kawamura@gmail.com>
2023-01-28 01:49:19 +0900

    for split

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   entry: './src/js/main.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   devServer: {
