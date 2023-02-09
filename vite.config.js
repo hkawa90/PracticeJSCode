ed0aa6eb0cb9a9d5d2f370d68ce46bb60fd720e5
hkawa90 <hideo.90.kawamura@gmail.com>
2023-02-09 22:05:47 +0900

    use rollup-plugin-visualizer for code split

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,55 @@
 import { defineConfig } from "vite"
 import autoprefixer from "autoprefixer"
-import path from 'path'
+import path, { resolve } from 'path'
 import babel from 'vite-plugin-babel'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command, mode }) => {
+  const resolve = {
+    resolve: {
+      alias: {
+        '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        '~vis-timeline': path.resolve(__dirname, 'node_modules/vis-timeline'),
+      }
+    }
+  }
+  const plugins = {
+    plugins: [
+      // Babel will try to pick up Babel config files (.babelrc or .babelrc.json)
+      babel(),
+      // ...
+    ]
+  }
+  const analyze = visualizer({
+    open: true,
+    filename: path.resolve(__dirname, 'dist/stats.html'),
+    gzipSize: true,
+    brotliSize: true,
+  })
+  const build =
+  {
+    build: {
+      rollupOptions: {
+        plugins: [
+          (mode === 'analyze') ? analyze : null
+        ]
+      }
+    }
+  }
+  const css = {
+    postcss: {
+      plugins: [autoprefixer],
+    }
+  }
+  console.log('vite:', Object.assign(resolve, plugins, css, build))
   if (command === "serve") {
+    const server = { server: { port: 8000 } }
     //開発環境設定
-    return {
-      resolve: {
-        alias: {
-          '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-          '~vis-timeline': path.resolve(__dirname, 'node_modules/vis-timeline'),
-        }
-      },
-      plugins: [
-        // Babel will try to pick up Babel config files (.babelrc or .babelrc.json)
-        babel(),
-        // ...
-      ],
-      server: {
-        port: 8000,
-      },
-    };
+    return Object.assign(resolve, plugins, server, build)
+
   } else {
     //本番環境設定
-    return {
-      resolve: {
-        alias: {
-          '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-          '~vis-timeline': path.resolve(__dirname, 'node_modules/vis-timeline'),
-        }
-      },
-      plugins: [
-        // Babel will try to pick up Babel config files (.babelrc or .babelrc.json)
-        babel(),
-        // ...
-      ],
-      css: {
-        postcss: {
-          plugins: [autoprefixer],
-        },
-      },
-    };
+    return Object.assign(resolve, plugins, css, build)
   }
 });
 
